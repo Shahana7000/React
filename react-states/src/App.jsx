b356881542e665803d8f176491496f8790954a78
Shahana700 <shahanacs2003@gmail.com>
2025-09-08 16:15:28 +0530

    react state and hooks

---
+++ react-states/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Counter from "./Counter.jsx"
+
+function App() {
+  return(
+    <div>
+  <>
+  <h1>States in React </h1>
+  <Counter/>
+  </>
+  </div>
+  )
+
+}
+
+export default App
---
+++ react-states/src/Counter.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+export default function Counter(){
+
+    let [count, setCount] = useState(0)
+ 
+    function incCount(){
+        // count =+ 1;
+        setCount(count + 1)
+        // console.log(count);
+
+    }
+
+    return(
+        <div>
+            <h3>Count = {count} </h3>
+            <button onClick={incCount}>increase count</button>
+        </div>
+    )
+}
+
+// export default function increaseCount(){
+
+// }
